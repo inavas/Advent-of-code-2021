6a3de2aff515943da14c1a37531a5777afabedbb
Inma Navas <immanavas@gmail.com>
2021-12-01 22:48:42 +0100

    calculate measurements from input file

--- src/test/kotlin/DepthMeasurementShould.kt
+++ src/test/kotlin/DepthMeasurementShould.kt
@@ -1,11 +1,11 @@
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
+import java.io.File
 
 class DepthMeasurementShould {
 
     @Test
     fun `Calculate how quickly depth increases`(){
-
         val measurements = listOf(199,200,208,210,200,207,240,269,260,263)
 
         val depthMeasurement = DepthMeasurement()
@@ -13,6 +13,23 @@ class DepthMeasurementShould {
         val result = depthMeasurement.calculate(measurements)
 
         assertThat(result).isEqualTo(7)
-
     }
+
+    @Test
+    fun `Calculate how quickly depth increases from input file`(){
+        val measurements = getIntListFromFile("depth_input_test.txt")
+
+        val depthMeasurement = DepthMeasurement()
+
+        val result = depthMeasurement.calculate(measurements)
+
+        assertThat(result).isEqualTo(7)
+    }
+
+    private fun getIntListFromFile(fileName: String): List<Int> {
+        val uri = this.javaClass.classLoader.getResource(fileName)
+
+        return File(uri.file).useLines { it.toList() }.map { it.toInt() }
+    }
+
 }
