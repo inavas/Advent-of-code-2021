f8c7375f7ed8aad63ecc014056c82fb8a1c67d3d
Inma Navas <immanavas@gmail.com>
2021-12-02 17:19:58 +0100

    calculate horizontal position and depth

--- src/main/kotlin/day2/SubmarineMovement.kt
+++ src/main/kotlin/day2/SubmarineMovement.kt
@@ -3,7 +3,20 @@ package day2
 class SubmarineMovement {
 
     fun calculate(movements: List<String>): Pair<Int, Int> {
-        TODO("Not yet implemented")
+
+        val horizontalPosition = movements.filter { command -> command.contains("forward") }
+                                        .map { value -> value.last().toString().toInt() }
+                                        .sum()
+
+        var depthDown = movements.filter { command -> command.contains("down") }
+            .map { value -> value.last().toString().toInt() }
+            .sum()
+
+        val depthUp = movements.filter { command -> command.contains("up") }
+            .map { value -> value.last().toString().toInt() }
+            .sum()
+
+        return Pair(horizontalPosition,depthDown-depthUp)
     }
 
 
