86521f009c74a22846a273416edb6e48fce79b3d
Inma Navas <immanavas@gmail.com>
2021-12-07 22:33:35 +0100

    Calculate breeding cycle of lanternfish

--- src/main/kotlin/day6/LanternfishLifeCycle.kt
+++ src/main/kotlin/day6/LanternfishLifeCycle.kt
@@ -2,10 +2,21 @@ package day6
 
 class LanternfishLifeCycle {
     fun calculateBreeding(initialLanternfishInput: List<String>, days: Int): List<Int> {
-        val initialLanternfish = initialLanternfishInput.first().split(",").map { it.toString().toInt() }
+        var initialLanternfish = initialLanternfishInput.first().split(",").map { it.toString().toInt() }.toMutableList()
 
 
-        return initialLanternfish.map { it-1 }
+        for (day in 0 until days) {
+            for (i in 0 until initialLanternfish.size) {
+                if (initialLanternfish[i] == 0) {
+                    initialLanternfish[i].apply { initialLanternfish[i] = 6 }
+                    initialLanternfish.add(8)
+                } else {
+                    initialLanternfish[i].apply { initialLanternfish[i] = initialLanternfish[i] - 1 }
+                }
+            }
+        }
+
+        return initialLanternfish
     }
 
 }
--- src/test/kotlin/day6/LanternfishLifeCycleShould.kt
+++ src/test/kotlin/day6/LanternfishLifeCycleShould.kt
@@ -1,6 +1,7 @@
 package day6
 
 import org.assertj.core.api.Assertions
+import org.assertj.core.api.Assertions.*
 import org.junit.jupiter.api.Test
 import java.io.File
 
@@ -15,9 +16,24 @@ class LanternfishLifeCycleShould {
         val resultingLanternfish = lanternfishLifeCycle.calculateBreeding(initialLanternfish, days)
 
         val expectedLanternfish = listOf(2,3,2,0,1)
-        Assertions.assertThat(resultingLanternfish).isEqualTo(expectedLanternfish)
-
+        assertThat(resultingLanternfish).isEqualTo(expectedLanternfish)
     }
+
+    @Test
+    fun `Calculate quantity of lanternfish after 18 days`(){
+        val initialLanternfish = getListFromFile("day6/lanternfish_input_test.txt")
+
+        val lanternfishLifeCycle = LanternfishLifeCycle()
+        val days = 18
+        val resultingLanternfish = lanternfishLifeCycle.calculateBreeding(initialLanternfish, days)
+        val totalFishes = resultingLanternfish.size
+
+        val expectedLanternfish = listOf(6,0,6,4,5,6,0,1,1,2,6,0,1,1,1,2,2,3,3,4,6,7,8,8,8,8)
+
+        assertThat(resultingLanternfish).isEqualTo(expectedLanternfish)
+        assertThat(totalFishes).isEqualTo(26)
+    }
+
 
     private fun getListFromFile(fileName: String): List<String> {
         val uri = this.javaClass.classLoader.getResource(fileName)
