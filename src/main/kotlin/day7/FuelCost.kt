340a693d24ae2619cde628135812340859fc8818
Inma Navas <immanavas@gmail.com>
2021-12-08 16:39:51 +0100

    calculate minimum fuel required to align crabs

--- src/main/kotlin/day7/FuelCost.kt
+++ src/main/kotlin/day7/FuelCost.kt
@@ -1,8 +1,18 @@
 package day7
 
+import kotlin.math.abs
+
 class FuelCost {
-    fun calculateMinimumFuelToAlign(crabsPosition: List<String>): Int {
-        TODO("Not yet implemented")
+    fun calculateMinimumFuelToAlign(crabsPositionInput: List<String>): Int {
+        val crabsPosition = crabsPositionInput.first().split(",").map { it.toInt() }.sorted()
+        val median: Int
+
+        val middle: Int = crabsPosition.size / 2
+        median = if (crabsPosition.size % 2 == 1) crabsPosition[middle]
+                else (crabsPosition[middle - 1] + crabsPosition[middle]) / 2
+
+
+        return crabsPosition.map { abs(it - median) }.sum()
     }
 
 }
