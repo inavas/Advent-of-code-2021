ad58ff5d0bb56ad31a27fdaa5ac1594f5058ad3d
Inma Navas <immanavas@gmail.com>
2021-12-21 23:10:41 +0100

    count how many times appears number 1 4 7 and 8

--- src/main/kotlin/day8/SevenSegmentDisplay.kt
+++ src/main/kotlin/day8/SevenSegmentDisplay.kt
@@ -2,7 +2,25 @@ package day8
 
 class SevenSegmentDisplay {
     fun countHowManyTimes1478Appears(signalInput: List<String>): Int {
-        TODO("Not yet implemented")
+        val numbersInput = signalInput.map { it.split("|") }
+        val numbers = numbersInput.map { it[1].trimStart().split(" ") }
+
+        var quantityOfNumbers = 0
+        for (number in numbers){
+            quantityOfNumbers += number.filter {
+                (isNumberOne(it)) or (isNumberFour(it)) or (isNumberSeven(it)) or (isNumberEight(it))
+            }.count()
+        }
+
+        return quantityOfNumbers
     }
+
+    private fun isNumberEight(it: String) = it.length == 7
+
+    private fun isNumberSeven(it: String) = it.length == 3
+
+    private fun isNumberFour(it: String) = it.length == 4
+
+    private fun isNumberOne(it: String) = it.length == 2
 
 }
